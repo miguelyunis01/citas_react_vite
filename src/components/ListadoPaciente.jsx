4998eead9d70819ff7c7d5cd938573ea59aedf93
miguelyunis01 <miguelyunis01@hotmail.com>
2022-12-15 19:26:19 -0500

    [ADD] se agrego formulario del paciente

---
+++ src/components/ListadoPaciente.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ListadoPaciente = () => {
+    return (
+        <div>
+            <h2>Listado Paciente</h2>
+        </div>
+    );
+};
+
+export default ListadoPaciente;
